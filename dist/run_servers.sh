9bf83593ed193de4a3539cf2120a40a01553638e
Allenhuangrui <rui.huang4@mail.mcgill.ca>
2021-11-28 10:48:24 -0500

    Added scripts to start the servers

--- dist/run_servers.sh
+++ dist/run_servers.sh
@@ -7,8 +7,8 @@ tmux new-session \; \
 	split-window -v \; \
 	select-layout main-vertical \; \
 	select-pane -t 0 \; \
-	send-keys "ssh -t ${MACHINES[0]} \"cd $(pwd) > /dev/null; echo -n 'Connected to '; hostname; .runsrvr.sh\"" C-m \; \
+	send-keys "ssh -t ${MACHINES[0]} \"cd $(pwd) > /dev/null; echo -n 'Connected to '; hostname; ./runsrvr.sh\"" C-m \; \
 	select-pane -t 1 \; \
-	send-keys "ssh -t ${MACHINES[1]} \"cd $(pwd) > /dev/null;; echo -n 'Connected to '; hostname; .runsrvr.sh\"" C-m \; \
+	send-keys "ssh -t ${MACHINES[1]} \"cd $(pwd) > /dev/null;; echo -n 'Connected to '; hostname; ./runsrvr.sh\"" C-m \; \
 	select-pane -t 2 \; \
-	send-keys "ssh -t ${MACHINES[2]} \"cd $(pwd) > /dev/null; echo -n 'Connected to '; hostname; .runsrvr.sh\"" C-m \;
+	send-keys "ssh -t ${MACHINES[2]} \"cd $(pwd) > /dev/null; echo -n 'Connected to '; hostname; ./runsrvr.sh\"" C-m \;
